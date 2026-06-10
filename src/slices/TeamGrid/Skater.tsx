b6a3b22b20fede2df7ceb6dc701a4e193d81ba4b
SANIO <krasulazr@gmail.com>
2026-06-10 18:46:18 +0300

    fix size photo

--- src/slices/TeamGrid/Skater.tsx
+++ src/slices/TeamGrid/Skater.tsx
@@ -22,25 +22,24 @@ export function Skater({ skater, index }: Props) {
 
   return (
     <div className="skater group relative flex flex-col items-center gap-4">
-      <div className="stack-layout overflow-hidden">
+      <div className="stack-layout relative w-full overflow-hidden aspect-square">
         <Image
           src={skater.photoBackground.src}
           alt={skater.photoBackground.alt}
-          width={500}
-          height={500}
-          quality={20}
-          className="scale-110 transform transition-all duration-1000 ease-in-out group-hover:scale-100 group-hover:brightness-75 group-hover:saturate-[.8]"
+          fill
+          quality={80}
+          className="object-cover scale-110 transform transition-all duration-1000 ease-in-out group-hover:scale-100 group-hover:brightness-75 group-hover:saturate-[.8]"
         />
-        <SkaterScribble className={clsx("relative", scribbleColor)} />
+        <SkaterScribble className={clsx("relative z-10 w-full h-full", scribbleColor)} />
         <Image
           src={skater.photoForeground.src}
           alt={skater.photoForeground.alt}
-          width={500}
-          height={500}
-          className="transform transition-transform duration-1000 ease-in-out group-hover:scale-110"
+          fill
+          style={{ scale: skater.foregroundScale ?? 1 }}
+          className="object-contain z-20 transform transition-transform duration-1000 ease-in-out group-hover:scale-110"
         />
-        <div className="relative h-48 w-full place-self-end bg-gradient-to-t from-black via-transparent to-transparent"></div>
-        <h3 className="relative grid place-self-end justify-self-start p-2 font-sans text-white ~text-2xl/3xl">
+        <div className="relative z-30 h-48 w-full place-self-end bg-gradient-to-t from-black via-transparent to-transparent"></div>
+        <h3 className="relative z-30 grid place-self-end justify-self-start p-2 font-sans text-white ~text-2xl/3xl">
           <span className="mb-[-.3em] block">{skater.firstName}</span>
           {skater.lastName && <span className="block">{skater.lastName}</span>}
         </h3>
