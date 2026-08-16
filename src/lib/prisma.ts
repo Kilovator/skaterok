b8e2aef01a75dd3c1b225217807bb459a66fe98d
Kilovator <dimonkrasula5@gmail.com>
2026-08-16 12:37:16 +0200

    prisma,fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,9 +4,18 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const databaseUrl =
+  process.env.DATABASE_URL ||
+  "postgresql://neondb_owner:npg_uLWdlAYezV57@ep-still-cell-ayh64c0u-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require";
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    datasources: {
+      db: {
+        url: databaseUrl,
+      },
+    },
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
